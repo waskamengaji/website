acf2f9d898275136cbf904de31d99439113ca2be
Abu Masyail <suluh.webdevelopers@hotmail.com>
2024-02-27 08:12:37 +0700

    feat: use vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,15 @@
-import adapter from '@sveltejs/adapter-auto'
+import adapter from '@sveltejs/adapter-vercel'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: [vitePreprocess()],
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      runtime: 'edge',
+      regions: 'sin1',
+      split: true,
+    }),
     alias: {
       $assets: 'src/assets',
       $components: 'src/components',
