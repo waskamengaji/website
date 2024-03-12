ef520e32876cda45908b4b4b2cd24bd9e5871fa2
Abu Masyail <suluh.webdevelopers@hotmail.com>
2024-03-12 15:58:28 +0700

    feat: add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { page } from '$app/stores'
+
+  let title = 'Terjadi Kesalahan'
+  let caption = 'Maaf, terjadi kesalahan tidak terduga pada sisi server.'
+
+  if ($page.status === 404) {
+    title = 'Halaman Tidak Ditemukan'
+    caption = 'Maaf, halaman yang kamu cari tidak dapat ditemukan.'
+  }
+</script>
+
+<svelte:head>
+  <title>{title}</title>
+</svelte:head>
+
+<div class="flex flex-col items-center text-center p-6 gap-1">
+  <h1 class="text-xl font-medium">{title}</h1>
+  <div class="flex gap-2 text-sm text-neutral-500">{caption}</div>
+</div>
