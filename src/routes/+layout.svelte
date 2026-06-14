2fdc3617fcdb9b0ec2020c5df49f518ed43c732b
Torch <20874779+sooluh@users.noreply.github.com>
2026-06-14 23:00:32 +0700

    chore: remove umami cloud script

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,13 +12,6 @@
   })
 </script>
 
-<svelte:head>
-  <script
-    defer
-    src="https://eu.umami.is/script.js"
-    data-website-id="deb7b694-7d6a-4f2b-a3f3-8d6378202879"></script>
-</svelte:head>
-
 <main class="pb-0">
   <slot />
 </main>
